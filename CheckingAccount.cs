207689e5c64d4bfcc73889ac5fe6b6dc836bf51d
RamaAwad21 <rama26awad2005@gmail.com>
2026-04-28 21:40:29 +0300

    Update CheckingAccount.cs

--- CheckingAccount.cs
+++ CheckingAccount.cs
@@ -8,7 +8,7 @@ namespace BankSystemProject
 {
     public class CheckingAccount : BankAccount
     {
-        private const double TransactionFee = 1.5;
+        private const double TransactionFee = 1.5; //تعريف ثابت لعمولة السحب وهي 1.5
         public CheckingAccount(string accNo, double initialBalance)
             : base(accNo, initialBalance)
         {
@@ -16,14 +16,14 @@ namespace BankSystemProject
         public override void Withdraw(double amount)
         {
             double totalAmount = amount + TransactionFee;
-            if (amount > 0 && Balance >= totalAmount)
+            if (amount > 0 && Balance >= totalAmount) //التحقق ان المبلغ المطلوب اكبر من الصفر
             {
-                Balance -= totalAmount;
-                Notify($"[Checking] Success! Withdrawn: {amount} + Fee: {TransactionFee}. New Balance: {Balance}");
+                Balance -= totalAmount; //خصم المبلغ الاجمالي من الرصيد
+                Notify($"[Checking] Success! Withdrawn: {amount} + Fee: {TransactionFee}. New Balance: {Balance}"); //ارسال اشعار بنجاح العملية مع تفاصيل الخصم والرصيدالجديد
             }
             else
             {
-                Notify("[Checking] Denied! Insufficient balance to cover the amount and the transaction fee.");
+                Notify("[Checking] Denied! Insufficient balance to cover the amount and the transaction fee."); //في حال عدم كفاية الرصيد يتم ارسال اشعار برفض العملية
             }
         }
     }
